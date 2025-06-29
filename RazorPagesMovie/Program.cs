60072515a1b85e7a41c24770e109662a499b7524
Ridha <fatmaridha999@gmail.com>
2025-06-29 09:13:05 +0300

    scaffolding

--- RazorPagesMovie/Program.cs
+++ RazorPagesMovie/Program.cs
@@ -5,6 +5,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddDbContext<RazorPagesMovieContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("RazorPagesMovieContext") ?? throw new InvalidOperationException("Connection string 'RazorPagesMovieContext' not found.")));
 
 if (builder.Environment.IsDevelopment())
 {
--- RazorPagesMovie/obj/Debug/net9.0/RazorPagesMovie.AssemblyInfo.cs
+++ RazorPagesMovie/obj/Debug/net9.0/RazorPagesMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1202904b35454acb0a90a6643610124502b98461")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b88be39fc62f93fa296600f62ba49cacaabc48f")]
 [assembly: System.Reflection.AssemblyProductAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
