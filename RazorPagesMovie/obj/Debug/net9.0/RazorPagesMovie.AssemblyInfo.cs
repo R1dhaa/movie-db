a09d3ddfa73799a5a4f90bc0d64f0cbcf1122c86
Ridha <fatmaridha999@gmail.com>
2025-07-01 12:53:19 +0300

    ui for index

--- RazorPagesMovie/obj/Debug/net9.0/RazorPagesMovie.AssemblyInfo.cs
+++ RazorPagesMovie/obj/Debug/net9.0/RazorPagesMovie.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7bac5d0cdbc6237f7c9e4f953f3e0d8511b76794")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04cfface9b85657f1fb7d7e276cec60a1c871e79")]
 [assembly: System.Reflection.AssemblyProductAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
